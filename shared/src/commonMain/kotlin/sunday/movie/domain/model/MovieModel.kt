426aae40da60a1d5408635a5cf3fd54439470258
Teresa <teresa.wu@tide>
2019-12-16 13:31:34 +0000

    move intergration logic from data class to mapper

--- shared/src/commonMain/kotlin/sunday/movie/domain/model/MovieModel.kt
+++ shared/src/commonMain/kotlin/sunday/movie/domain/model/MovieModel.kt
@@ -1,12 +1,9 @@
 package sunday.movie.domain.model
 
-private const val MOVIE_POSTER_BASE_URL = "https://image.tmdb.org/t/p/original"
-
 data class Movie(
     val id: Int,
     val title: String,
     val popularity: Double,
-    val posterPath: String
-) {
-    val completePosterPath = MOVIE_POSTER_BASE_URL + posterPath
-}
+    val posterPath: String,
+    val completePosterPath
+)
